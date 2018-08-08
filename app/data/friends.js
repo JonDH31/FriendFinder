cb62e1a7ef5c671f26367f3ff1134fbbb57fec10
Jon Stampley <jonstampley@gmail.com>
2018-08-07 22:47:12 -0500

    make it better

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+var friends = [
+
+    {
+        name: "Stacy",
+        photo: "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [
+            "3",
+            "5",
+            "1",
+            "2",
+            "4",
+            "2",
+            "3",
+            "2",
+            "1",
+            "3",
+        ]
+    },
+    {
+        name: "Sue",
+        photo: "https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [
+            "2",
+            "4",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "3",
+            "1",
+        ]
+    },
+    {
+        name: "Betsy",
+        photo: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [
+            "1",
+            "3",
+            "5",
+            "3",
+            "2",
+            "4",
+            "1",
+            "4",
+            "2",
+            "3",
+        ]
+    },
+    {
+        name: "Tina",
+        photo: "https://images.pexels.com/photos/1036622/pexels-photo-1036622.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [
+            "5",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+        ]
+    },
+    {
+        name: "Candace",
+        photo: "https://images.pexels.com/photos/372042/pexels-photo-372042.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [
+            "4",
+            "1",
+            "2",
+            "5",
+            "3",
+            "3",
+            "5",
+            "4",
+            "1",
+            "2",
+        ]
+    },
+    {
+        name: "Meghan",
+        photo:"https://images.pexels.com/photos/756453/pexels-photo-756453.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [
+            "3",
+            "5",
+            "3",
+            "1",
+            "2",
+            "4",
+            "3",
+            "2",
+            "1",
+            "4",
+        ]
+    },
+    {
+        name: "Erica",
+        photo: "https://images.pexels.com/photos/1036620/pexels-photo-1036620.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [
+            "2",
+            "4",
+            "5",
+            "3",
+            "1",
+            "1",
+            "5",
+            "5",
+            "1",
+            "2",
+        ]
+    },
+    {
+        name: "Eve",
+        photo: "https://images.pexels.com/photos/794064/pexels-photo-794064.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [
+            "1",
+            "3",
+            "1",
+            "5",
+            "4",
+            "2",
+            "4",
+            "5",
+            "3",
+            "5",
+        ]
+    },
+    {
+        name: "Pamela",
+        photo: "https://images.pexels.com/photos/264172/pexels-photo-264172.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [
+            "5",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "1",
+            "3",
+            "1",
+            "4",
+        ]
+    },
+    {
+        name: "Nicole",
+        photo: "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [
+            "4",
+            "1",
+            "2",
+            "3",
+            "5",
+            "5",
+            "1",
+            "4",
+            "3",
+            "2",
+        ]
+    }
+];
+
+module.exports = friends;
